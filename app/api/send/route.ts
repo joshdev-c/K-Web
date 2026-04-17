d8cfedf4f1337579b020b7d358fea4c6c7b0b290
Joshua <digitals.joshcst@gmail.com>
2026-04-17 15:46:35 +0800

    updated route

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,8 +1,8 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(request: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
   const { name, email, subject, message } = await request.json();
   try {
     const data = await resend.emails.send({
